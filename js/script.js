0a9bc938d9d5f772b76c41f760d02a87b74efd69
thu le <dangtrunghieu123@gmail.com>
2018-12-08 12:44:15 +0700

    multi

---
+++ js/script.js
@@ -0,0 +1,93 @@
+$(document).ready(function(){
+  
+    $(window).on('scroll',function(){
+        console.log(window.pageYOffset);
+        var sticky = $('.demo').offset().top;
+        console.log(sticky);
+        if(window.pageYOffset > sticky){
+            $('.demo').addClass("sticky");
+        }
+        else  
+        {
+            $('.demo').removeClass("sticky");
+        }
+    })
+    $('.nav2 ul li').on('click',function(){
+        $('.nav2 ul li').removeClass('active');
+        $(this).addClass('active');
+        
+    });
+
+    $('.recent .eventProject .event button').on('click',function(){
+        var show = $(this).attr('class');//get value of propeties
+      
+        $('.recent .eventProject .event button').removeClass('.recent .eventProject .event active');
+        $(this).addClass('.recent .eventProject .event active');
+       
+        if(show == 'all'){
+            $('.grid-container').children('div.grid-item').show();
+        }
+        else{
+            $('.grid-container').children('div:not(.' +show+')').hide();
+            $('.grid-container').children('div.'+show).show();
+        }
+        return false;
+    });
+    $('#owlLogo').owlCarousel({
+        loop:true,
+        margin:10,
+        // nav:true,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    })
+    
+    $('#owlComment').owlCarousel({
+        loop:true,
+        margin:10,
+        // nav:true,
+        autoplay:true,
+        autoplayTimeout:2000,
+        autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
+    // var obj = [
+    //     {img:'../img/pic-1.jpg'},
+    //     {img:'../img/pic-2.jpg'},
+    //     {img:'../img/pic-3.jpg'},
+    //     {img:'../img/pic-4.jpg'},
+    //     {img:'../img/pic-5.jpg'},
+    //     {img:'../img/pic-6.jpg'},
+    //     {img:'../img/pic-7.jpg'},
+    //     {img:'../img/pic-8.jpg'},
+    //     {img:'../img/pic-9.jpg'},
+    //     {img:'../img/pic-10.jpg'},
+    //     {img:'../img/pic-11.jpg'},
+    //     {img:'../img/pic-12.jpg'},
+    // ]
+    // for(var i= 0; i< obj.length;i++){
+        
+    // }
+
+})
